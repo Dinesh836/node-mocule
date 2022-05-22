b116a058ab4d5cd36023bfffee8fbe410d9d93f7
Dinesh836 <103949855+Dinesh836@users.noreply.github.com>
2022-05-22 22:04:23 +0530

    module in node

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const http = require('http');
+const express=require('express');
+const bodyParser=require('body-parser')
 
-const routes=require('./routes');
-const server = http.createServer(routes.handler);
-server.listen(3000);
+const app = express();
+
+
+const adminRoute=require('./routes/admin')
+const shopRutes=require('./routes/shop')
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(adminRoute);
+app.use(shopRutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h2>page not fount</h2>')
+})
+
+app.listen(1000);
